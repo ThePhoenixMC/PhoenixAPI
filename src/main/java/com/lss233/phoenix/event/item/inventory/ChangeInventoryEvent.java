4be0098db00269c35385bffade6f2c98ec9e9f5e
lss233 <m18760286457@163.com>
2017-12-30 15:05:11 +0800

    Add ChangeInventoryEvent
    Signed-off-by: lss233 <m18760286457@163.com>

---
+++ src/main/java/com/lss233/phoenix/event/item/inventory/ChangeInventoryEvent.java
@@ -0,0 +1,34 @@
+package com.lss233.phoenix.event.item.inventory;
+
+import com.lss233.phoenix.event.Cancellable;
+
+/**
+ * Represents an event fired when a Living changed its inventory
+ * Cause:
+ * <ul>
+ *     <li>player - The player.</li>
+ *     <li>item - The item.</li>
+ * </ul>
+ */
+public interface ChangeInventoryEvent extends Cancellable {
+    /**
+     * Fired when a Living changes it's equipment.
+     */
+    interface Equipment extends  ChangeInventoryEvent{
+
+    }
+
+    /**
+     * Fired when a Living changes it's held ItemStack.
+     */
+    interface Held extends ChangeInventoryEvent{
+
+    }
+
+    /**
+     * Fired when a Living picked up a ItemStack
+     */
+    interface Pickup extends ChangeInventoryEvent{
+
+    }
+}
