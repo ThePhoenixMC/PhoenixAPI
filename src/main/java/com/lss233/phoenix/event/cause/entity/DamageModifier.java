99ab67d5271c2dbb7f56e53e9fd6c3d9c8f8c7a7
lss233 <m18760286457@163.com>
2017-12-30 14:56:35 +0800

    Add DamageEntityEvent (incomplete)
    Signed-off-by: lss233 <m18760286457@163.com>

---
+++ src/main/java/com/lss233/phoenix/event/cause/entity/DamageModifier.java
@@ -0,0 +1,18 @@
+package com.lss233.phoenix.event.cause.entity;
+
+/**
+ *
+ */
+public class DamageModifier {
+    static class Builder {
+
+    }
+
+    /**
+     * Creates a new {@link com.lss233.phoenix.event.cause.entity.DamageModifier.Builder}. for constructing a {@link com.lss233.phoenix.event.cause.entity.DamageModifier}.
+     * @return A new builder
+     */
+    public static Builder builder(){
+        return new Builder();
+    }
+}
--- src/main/java/com/lss233/phoenix/event/entity/DamageEntityEvent.java
+++ src/main/java/com/lss233/phoenix/event/entity/DamageEntityEvent.java
@@ -1,7 +1,30 @@
 package com.lss233.phoenix.event.entity;
 
+import com.lss233.phoenix.event.Cancellable;
+import com.lss233.phoenix.event.cause.entity.DamageModifier;
+
 /**
- *
+ * Represents the base event for when an Entity is being "attacked".
  */
-public interface DamageEntityEvent {
+public interface DamageEntityEvent extends Cancellable {
+
+    /**
+     * Gets the raw amount of damage caused by the event
+     * @return The raw amount of damage caused by the event
+     */
+    double getDamage();
+
+    /**
+     * Gets the amount of damage caused by the event after all damage reduction is applied.
+     * @return The amount of damage caused by the event
+     */
+    double getFinalDamage();
+
+    /**
+     * Gets the damage for the provide {@link com.lss233.phoenix.event.entity.DamageEntityEvent}.
+     * @param damageModifier
+     * @return The raw amount of damage caused by the event
+     */
+    double getDamage(DamageModifier damageModifier);
+
 }
