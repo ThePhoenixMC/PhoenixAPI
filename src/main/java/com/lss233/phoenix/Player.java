953fb7b930cc8fc2b028462f37a2adc2e9f54c73
thttnt <848815969@qq.com>
2018-01-03 23:11:57 +0800

    update LivingEntity Class

--- src/main/java/com/lss233/phoenix/Player.java
+++ src/main/java/com/lss233/phoenix/Player.java
@@ -3,9 +3,10 @@ package com.lss233.phoenix;
 import com.lss233.phoenix.channel.PluginMessageRecipient;
 import com.lss233.phoenix.command.CommandSender;
 import com.lss233.phoenix.entity.Entity;
+import com.lss233.phoenix.entity.LivingEntity;
 
 /**
  * .
  */
-public interface Player extends Entity, OfflinePlayer, CommandSender, PluginMessageRecipient {
+public interface Player extends LivingEntity, OfflinePlayer, CommandSender, PluginMessageRecipient {
 }
--- src/main/java/com/lss233/phoenix/entity/LivingEntity.java
+++ src/main/java/com/lss233/phoenix/entity/LivingEntity.java
@@ -3,5 +3,11 @@ package com.lss233.phoenix.entity;
 /**
  *
  */
-public interface LivingEntity {
+public interface LivingEntity extends Entity{
+
+    double getHealth();
+
+    double setHealth(double health);
+
+
 }
