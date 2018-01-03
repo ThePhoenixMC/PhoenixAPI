8eb55dc58b7d3ce00ff7469bc8b5ce40f9ffb422
thttnt <848815969@qq.com>
2018-01-03 23:29:33 +0800

    add teleport method in class Entity

--- src/main/java/com/lss233/phoenix/entity/Entity.java
+++ src/main/java/com/lss233/phoenix/entity/Entity.java
@@ -8,5 +8,19 @@ import com.lss233.phoenix.Location;
 public interface Entity {
     Location getLocation();
 
+    /**
+     * teleport to position
+     * @param location position
+     * @return return true if teleport is successful
+     */
+    boolean teleport(Location location);
+
+    /**
+     * teleport to entity
+     * @param entity entity
+     * @return return true if teleport is successful
+     */
+    boolean teleport(Entity entity);
+
 
 }
