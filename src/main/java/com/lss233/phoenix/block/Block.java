bfe1828b0d7a00ce385b96789db45fe40359f9d9
thttnt <848815969@qq.com>
2018-01-04 22:03:11 +0800

    add Class BlockState update Class Block

--- src/main/java/com/lss233/phoenix/block/Block.java
+++ src/main/java/com/lss233/phoenix/block/Block.java
@@ -1,7 +1,13 @@
 package com.lss233.phoenix.block;
 
+import com.lss233.phoenix.Location;
+
 /**
  * .
  */
 public interface Block {
+
+    Location getLocation(Location location);
+
+    BlockState getState(BlockState state);
 }
---
+++ src/main/java/com/lss233/phoenix/block/BlockState.java
@@ -0,0 +1,7 @@
+package com.lss233.phoenix.block;
+
+import com.lss233.phoenix.Location;
+
+public interface BlockState {
+
+}
