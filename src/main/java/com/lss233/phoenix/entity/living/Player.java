8dfe24411466000f591ac62d176f45c761321517
lss233 <m18760286457@163.com>
2018-01-07 16:36:29 +0800

    Delete OfflinePlayer class, we will implement it later.
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/entity/living/Player.java
+++ src/main/java/com/lss233/phoenix/entity/living/Player.java
@@ -1,13 +1,11 @@
 package com.lss233.phoenix.entity.living;
 
-import com.lss233.phoenix.OfflinePlayer;
 import com.lss233.phoenix.channel.PluginMessageRecipient;
 import com.lss233.phoenix.command.CommandSender;
-import com.lss233.phoenix.entity.Entity;
 import com.lss233.phoenix.entity.LivingEntity;
 
 /**
  * A Player represents the in-game entity of a human playing on a server.
  */
-public interface Player extends LivingEntity, OfflinePlayer, CommandSender, PluginMessageRecipient {
+public interface Player extends LivingEntity, CommandSender, PluginMessageRecipient {
 }
