9a2c075be66219721363eb23a869dbc175081049
lss233 <m18760286457@163.com>
2018-01-27 23:38:34 +0800

    Update Player
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/entity/living/Player.java
+++ src/main/java/com/lss233/phoenix/entity/living/Player.java
@@ -8,4 +8,14 @@ import com.lss233.phoenix.entity.LivingEntity;
  * A Player represents the in-game entity of a human playing on a server.
  */
 public interface Player extends LivingEntity, CommandSender, PluginMessageRecipient {
+    /**
+     * Gets the player's last known username.
+     * @return The player's last known username
+     */
+    String getName();
+
+    /**
+     * Kicks the player.
+     */
+    void kick();
 }
