858e89b913f9717937c117066760a34f2b3e4777
lss233 <m18760286457@163.com>
2018-01-31 21:35:30 +0800

    Update command system.
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/builtin/commands/PhoenixCommandExecutor.java
+++ src/main/java/com/lss233/phoenix/builtin/commands/PhoenixCommandExecutor.java
@@ -9,7 +9,7 @@ import com.lss233.phoenix.command.*;
 @PhoenixCommand(label = "phoenix", description = "Phoenix built-in commands", permission = "phoenix.admin", alias = {"p"})
 public class PhoenixCommandExecutor implements Command {
     @CommandRouter(args = "help", permission = "phoenix.admin.help")
-    public boolean help(CommandSender sender, String label, ArgumentsMap args) {
+    public boolean help(CommandSender sender, CommandContent args) {
         sender.sendMessage("          Phoenix Framework          ");
         sender.sendMessage("-------------------------------------");
         sender.sendMessage("/phoenix help    -    Show help menu.");
@@ -18,16 +18,16 @@ public class PhoenixCommandExecutor implements Command {
         return true;
     }
     @CommandRouter(args = "reload", permission = "phoenix.admin.reload")
-    public boolean reload(CommandSender sender, String label, ArgumentsMap args) {
+    public boolean reload(CommandSender sender, String label, CommandContent args) {
         return true;
     }
     @CommandRouter(args = "version", permission = "phoenix.admin.version")
-    public boolean version(CommandSender sender, String label, ArgumentsMap args) {
+    public boolean version(CommandSender sender, String label, CommandContent args) {
         sender.sendMessage("Server is running on " + Phoenix.getServer().getVersion() + " with Phoenix " + Phoenix.getVersion());
         return true;
     }
     @Override
     public boolean onMissHandled(CommandSender sender, String label, String[] args) {
-        return help(sender, label, null);
+        return help(sender, null);
     }
 }
