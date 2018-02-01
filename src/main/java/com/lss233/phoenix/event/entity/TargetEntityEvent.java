c8b994efa9ce760c0b1c8b01036cc9de08a68f69
lss233 <m18760286457@163.com>
2018-02-01 14:16:34 +0800

    Update player events.
    Signed-off-by: lss233 <m18760286457@163.com>

---
+++ src/main/java/com/lss233/phoenix/event/entity/TargetEntityEvent.java
@@ -0,0 +1,14 @@
+package com.lss233.phoenix.event.entity;
+
+import com.lss233.phoenix.entity.Entity;
+
+/**
+ *
+ */
+public interface TargetEntityEvent {
+    /**
+     * Gets the target {@link Entity}.
+     * @return The entity.
+     */
+    Entity 	getTargetEntity();
+}
