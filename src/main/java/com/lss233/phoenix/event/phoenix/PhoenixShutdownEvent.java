a3e8a58cd36e4dbb3f308a64df279b415c6e718a
lss233 <m18760286457@163.com>
2018-02-01 14:16:45 +0800

    Update Phoenix events.
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/event/phoenix/PhoenixShutdownEvent.java
+++ src/main/java/com/lss233/phoenix/event/phoenix/PhoenixShutdownEvent.java
@@ -3,5 +3,8 @@ package com.lss233.phoenix.event.phoenix;
 
 import com.lss233.phoenix.event.Event;
 
+/**
+ * Triggered when Phoenix is about to shutdown.
+ */
 public interface PhoenixShutdownEvent extends Event {
 }
