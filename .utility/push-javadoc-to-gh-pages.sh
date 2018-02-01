3eb6b91b641540791bf3260f6c78273b05b93672
lss233 <m18760286457@163.com>
2018-02-01 11:57:09 +0800

    Update travis-ci config.
    Signed-off-by: lss233 <m18760286457@163.com>

---
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# https://github.com/ReadyTalk/swt-bling/tree/master/.utility
+if [ "$TRAVIS_REPO_SLUG" == "ThePhoenixMC/PhoenixAPI" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R target/apidocs $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet https://${GH_TOKEN}@ThePhoenixMC/ThePhoenixMC.github.io.git gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin master > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+
+fi
