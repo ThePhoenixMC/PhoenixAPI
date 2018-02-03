5f278f17bfaffc743075cf039c3f4484186cd4a5
thttnt <848815969@qq.com>
2018-02-04 02:12:35 +0800

    Improve phoenix.block

--- src/main/java/com/lss233/phoenix/event/block/ChangeBlockEvent.java
+++ src/main/java/com/lss233/phoenix/event/block/ChangeBlockEvent.java
@@ -1,5 +1,6 @@
 package com.lss233.phoenix.event.block;
 
+import com.lss233.phoenix.block.Block;
 import com.lss233.phoenix.event.Cancellable;
 import com.lss233.phoenix.event.Event;
 
@@ -11,11 +12,19 @@ public interface ChangeBlockEvent extends Event, Cancellable {
      * <p>Called when a block is broken.or moved.</p>
      * <p>Cause:</p>
      * <ul>
-     *     <li>player - The Player that is breaking the block involved in this event.</li>
+     * <li>player - The Player that is breaking the block involved in this event.</li>
      * </ul>
      */
-    interface Break extends  ChangeBlockEvent {}
-    interface Decay extends  ChangeBlockEvent {}
-    interface Grow extends  ChangeBlockEvent {}
-    interface Modify extends  ChangeBlockEvent {}
+    interface Break extends ChangeBlockEvent {
+    }
+
+    interface Decay extends ChangeBlockEvent {
+    }
+
+    interface Grow extends ChangeBlockEvent {
+    }
+
+    interface Modify extends ChangeBlockEvent {
+    }
+
 }
