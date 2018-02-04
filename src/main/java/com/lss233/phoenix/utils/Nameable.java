2da31624147a747681691417a14682d836eb7487
lss233 <m18760286457@163.com>
2018-02-04 12:06:58 +0800

    Update Nameable

---
+++ src/main/java/com/lss233/phoenix/utils/Nameable.java
@@ -0,0 +1,12 @@
+package com.lss233.phoenix.utils;
+
+/**
+ * An object which has a name.
+ */
+public interface Nameable {
+    /**
+     * Gets the human-readable name of this object.
+     * @return The name of this object
+     */
+    String getName();
+}
