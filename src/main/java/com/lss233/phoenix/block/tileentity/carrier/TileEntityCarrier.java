6a796ca4ff66ecf6688d6f6f71bb801ccfb4fd92
lss233 <m18760286457@163.com>
2018-02-05 13:53:38 +0800

    Update tiles.
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/block/tileentity/carrier/TileEntityCarrier.java
+++ src/main/java/com/lss233/phoenix/block/tileentity/carrier/TileEntityCarrier.java
@@ -1,12 +1,13 @@
-package com.lss233.phoenix.item.inventory.tileentity.carrier;
+package com.lss233.phoenix.block.tileentity.carrier;
 
 import com.lss233.phoenix.item.inventory.Carrier;
 import com.lss233.phoenix.item.inventory.TileEntityInventory;
+import com.lss233.phoenix.block.tileentity.TileEntity;
 
 /**
  *
  */
-public interface TileEntityCarrier extends Carrier{
+public interface TileEntityCarrier extends TileEntity,Carrier{
     /**
      * Returns the inventory that this Carrier is holding.
      * @return The inventory this Carrier is holding
