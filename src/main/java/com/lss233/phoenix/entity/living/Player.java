28cbf802b761b3496d643e0fd3626a31b49f0599
lss233 <m18760286457@163.com>
2018-02-05 22:55:38 +0800

    Add Equipable for entity.
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/entity/living/Player.java
+++ src/main/java/com/lss233/phoenix/entity/living/Player.java
@@ -2,12 +2,13 @@ package com.lss233.phoenix.entity.living;
 
 import com.lss233.phoenix.channel.PluginMessageRecipient;
 import com.lss233.phoenix.command.CommandSender;
+import com.lss233.phoenix.entity.Equipable;
 import com.lss233.phoenix.entity.LivingEntity;
 
 /**
  * A Player represents the in-game entity of a human playing on a server.
  */
-public interface Player extends LivingEntity, CommandSender, PluginMessageRecipient {
+public interface Player extends LivingEntity, CommandSender, PluginMessageRecipient, Equipable {
     /**
      * Gets the player's last known username.
      * @return The player's last known username
---
+++ src/main/java/com/lss233/phoenix/item/inventory/equipment/EquipmentType.java
@@ -0,0 +1,19 @@
+package com.lss233.phoenix.item.inventory.equipment;
+
+/**
+ * Equipment types.
+ */
+public enum EquipmentType {
+    /*
+    Wear
+     */
+    BOOTS,
+    CHESTPLATE,
+    HEADWEAR,
+    LEGGINGS,
+    /*
+    Hold
+     */
+    MAIN_HAND,
+    OFF_HAND,
+}
