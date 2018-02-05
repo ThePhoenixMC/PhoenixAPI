1d4dfb79f60c780a3af6074bb0ea1ca5c2505196
lss233 <m18760286457@163.com>
2018-02-05 13:55:34 +0800

    Update Block Carrier.
    Signed-off-by: lss233 <m18760286457@163.com>

--- src/main/java/com/lss233/phoenix/block/tileentity/carrier/TileEntityCarrier.java
+++ src/main/java/com/lss233/phoenix/block/tileentity/carrier/TileEntityCarrier.java
@@ -1,5 +1,6 @@
 package com.lss233.phoenix.block.tileentity.carrier;
 
+import com.lss233.phoenix.item.inventory.BlockCarrier;
 import com.lss233.phoenix.item.inventory.Carrier;
 import com.lss233.phoenix.item.inventory.TileEntityInventory;
 import com.lss233.phoenix.block.tileentity.TileEntity;
@@ -7,7 +8,7 @@ import com.lss233.phoenix.block.tileentity.TileEntity;
 /**
  *
  */
-public interface TileEntityCarrier extends TileEntity,Carrier{
+public interface TileEntityCarrier extends TileEntity,BlockCarrier{
     /**
      * Returns the inventory that this Carrier is holding.
      * @return The inventory this Carrier is holding
