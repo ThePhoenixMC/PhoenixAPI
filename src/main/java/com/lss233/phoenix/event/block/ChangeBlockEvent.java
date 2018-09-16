cb9ecc28f519aac686d3ff9a8b7cfe9555fe8078
lss233 <ls1102086624@outlook.com>
2018-09-16 17:45:38 +0800

    Update ChangeBlockEvent
    Signed-off-by: lss233 <ls1102086624@outlook.com>

--- src/main/java/com/lss233/phoenix/event/block/ChangeBlockEvent.java
+++ src/main/java/com/lss233/phoenix/event/block/ChangeBlockEvent.java
@@ -4,26 +4,38 @@ import com.lss233.phoenix.event.Cancellable;
 import com.lss233.phoenix.event.Event;
 
 /**
- * <p>Represents an event fired when the state of a Block changed.</p>
+ * Represents an event fired when the state of a Block changed.
  */
 public interface ChangeBlockEvent extends Event, Cancellable {
     /**
-     * <p>Called when a block is broken.or moved.</p>
-     * <p>Cause:</p>
-     * <ul>
-     * <li>player - The Player that is breaking the block involved in this event.</li>
-     * </ul>
+     * Fired when a block is broken or moved.
      */
     interface Break extends ChangeBlockEvent {
     }
 
+    /**
+     * Fired when a block is decayed.
+     */
     interface Decay extends ChangeBlockEvent {
     }
 
+    /**
+     * Fired when a block is grown.
+     */
     interface Grow extends ChangeBlockEvent {
     }
 
+    /**
+     * Fired when a block type is modified
+     */
     interface Modify extends ChangeBlockEvent {
+    }
+
+    /**
+     * Fired when one or more {@link com.lss233.phoenix.block.Block}s are added to the {@link com.lss233.phoenix.world.World}.
+     */
+    interface Place extends ChangeBlockEvent {
+
     }
 
 }
